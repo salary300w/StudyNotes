db38e7515054a61f8babdb2bd66276f862e143c0
lenovo-win <lenovo-win>
2022-12-13 11:59:47 +0800

    加入QtInstall.sh文件

---
+++ QtInstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt install libxcb-glx0 -y
+apt install libx11-xcb1 -y
+apt install libxcb-icccm4 -y
+apt install libxcb-image0 -y
+apt install libxcb-keysyms1 -y
+apt install libxcb-randr0 -y
+apt install libxcb-render-util0 -y
+apt install libxcb-shape0 -y
+apt install libxcb-sync1 -y
+apt install libxcb-xfixes0 -y
+apt install libxcb-xinerama0 -y
+apt install libxcb-xkb1 -y
+apt install libxkbcommon-x11-0 -y
+apt install libgl1 -y
+apt install libfontconfig1 -y
